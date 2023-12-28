7b1ebfa458e3f812729b8198a82cae9dc459d4fa
Elaine-Chagas <152560000+Elaine-Chagas@users.noreply.github.com>
2023-12-28 23:45:30 +0000

    [feat]:criei um repositorio

--- draft.py
+++ draft.py
@@ -1,2 +1,18 @@
-from plot.static import plot_line
-plot_line('BTC-USD')
+
+from data.download import download_data
+
+data = download_data('BBAS3.SA')
+
+data[['Close']]
+data['SMA'] = data['Close'].rolling(window = 9).mean()
+data['LMA'] = data['Close'].rolling(window = 72).mean()
+
+import plotly.express as px
+
+fig = px.line(
+    data.reset_index(),
+    x= 'Date', y=['Close', 'SMA', 'LMA'], title='BBAS3',
+    labels={'Close': 'Fechamento', 'Date': 'Data'},
+    color_discrete_map={'Close': 'black', 'SMA': 'blue', 'LMA': 'red'}
+)
+fig.show()
